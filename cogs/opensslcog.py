53a7d5b50e2f2fcd3ec70d47d5c55cfd5988a343
Pelda03 <110687291+Pelda03@users.noreply.github.com>
2023-08-24 11:21:42 +0200

    hb

--- cogs/opensslcog.py
+++ cogs/opensslcog.py
@@ -9,12 +9,16 @@ class OpenSSLCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def run_openssl(self, ctx, *, command):
-        try:
-            result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True)
-            await ctx.send("COmmand executed successfully:\n```\n" + result)
-        except subprocess.CalledProcessError as e:
-            await ctx.send("Command failed:\n```\n" + e.output) + "\n```" 
+
+# Spustí script, jeste neni přidaná cesta na scripty
+@commands.command()
+async def run_openssl(self, ctx):
+    try:
+        result = subprocess.check_output(['openssl', 'jmeno_ssl_scriptu'], shell=True, stderr=subprocess.STDOUT, text=True)
+        await ctx.send("Certificate:\n```\n" + result + "\n```")
+    except subprocess.CalledProcessError as e:
+        await ctx.send("Command failed:\n```\n" + e.output + "\n```")
+
 def setup(bot):
-    bot.add_cog(OpenSSLCog(bot))           
+    bot.add_cog(OpenSSLCog(bot))
+
